f4188686334882c664949e0cfeba74752bed3e56
ygdm7 <zx909@protonmail.com>
2017-10-31 16:01:17 +0000

    adds remaining examples from chapter 5

---
+++ chapter5/examples/5.6.c
@@ -0,0 +1,21 @@
+// Program 5.6
+// Program to introduce the while statement
+
+#include<stdio.h>
+
+int main( void )
+{
+
+    int count = 1;
+
+    while( count <= 5 )
+    {
+
+	printf( "%i\n", count );
+	++count;
+
+    }
+
+    return 0;
+
+}
---
+++ chapter5/examples/5.7.c
@@ -0,0 +1,29 @@
+// Program 5.7
+/* Program to find the greatest common divisor
+   of two nonnegative integer values
+*/
+
+#include<stdio.h>
+
+int main( void )
+{
+
+    int u, v, temp;
+
+    printf( "Please type in two nonegative integers.\n" );
+    scanf( "%i%i", &u, &v );
+
+    while( v != 0 )
+    {
+
+	temp = u % v;
+	u = v;
+	v = temp;
+
+    }
+
+    printf( "Their greatest common divisor is %i\n", u );
+
+    return 0;
+
+}
