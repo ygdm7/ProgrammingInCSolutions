160534e8071f3c54a69316c1a469e411e016d7c1
ygdm7 <zx909@protonmail.com>
2018-02-02 16:46:23 -0200

    adds 8 first examples from chapter 8

---
+++ chapter8/examples/8.1.c
@@ -0,0 +1,19 @@
+// Program 8.1
+
+#include<stdio.h>
+
+void printMessage( void )
+{
+
+    printf( "Programming is fun.\n" );
+
+}
+
+int main( void )
+{
+
+    printMessage();
+
+    return 0;
+
+}
---
+++ chapter8/examples/8.2.c
@@ -0,0 +1,20 @@
+// Program 8.2
+
+#include<stdio.h>
+
+void printMessage( void )
+{
+
+    printf( "Programming is fun.\n" );
+
+}
+
+int main( void )
+{
+
+    printMessage();
+    printMessage();
+
+    return 0;
+
+}
