53ae12907a5ee8443a8f0308445952d35baac728
ygdm7 <zx909@protonmail.com>
2018-02-09 11:07:26 -0200

    adds all chapter 9 examples

---
+++ chapter9/examples/9.1.c
@@ -0,0 +1,28 @@
+// Program 9.1
+// Program to illustrate a structure
+
+#include<stdio.h>
+
+int main( void )
+{
+
+    struct date
+    {
+
+	int month;
+	int day;
+	int year;
+
+    };
+
+    struct date today;
+
+    today.month = 9;
+    today.day = 25;
+    today.year = 2004;
+
+    printf( "Today's date i %i/%i/%.2i.\n", today.month, today.day, today.year % 100 );
+
+    return 0;
+
+}
