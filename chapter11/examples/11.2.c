515c7ca08bfebf9ed1d3459ffd29fa9070c74ec9
ygdm7 <zx909@protonmail.com>
2018-02-19 17:48:42 -0300

    adds 7 first examples from chapter 11

---
+++ chapter11/examples/11.2.c
@@ -0,0 +1,20 @@
+// Program 11.2
+// Further examples of pointers
+
+#include<stdio.h>
+
+int main( void )
+{
+    char c = 'Q';
+    char *char_pointer = &c;
+
+    printf( "%c %c\n", c, *char_pointer );
+
+    c = '/';
+    printf( "%c %c\n", c, *char_pointer );
+
+    *char_pointer = '(';
+    printf( "%c %c\n", c, *char_pointer );
+
+    return 0;
+}
