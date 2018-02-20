86ac35b43561dc3f38c30610d829d2c9f5254e64
ygdm7 <zx909@protonmail.com>
2018-02-20 15:59:45 -0300

    adds last 8 examples from chapter 11

---
+++ chapter11/examples/11.15.c
@@ -0,0 +1,25 @@
+// Program 11.15
+// Function to count the characters in a string - Pointer version
+
+#include<stdio.h>
+
+int stringLength( const char *string )
+{
+    const char *cptr = string;
+
+    while( *cptr )
+	++cptr;
+
+    return cptr - string;
+}
+
+int main( void )
+{
+    int stringLength( const char *string );
+
+    printf( "%i ", stringLength( "stringLength test" ) );
+    printf( "%i ", stringLenght( "" ) );
+    printf( "%i\n", stringLength( "complete" ) );
+
+    return 0;
+}
